45d473b0761eec3cbf271ed44345b2cc6a0c45ce
Damian <70946678+DamianvdD@users.noreply.github.com>
2021-11-12 12:43:56 +0100

    Aanpassingen aan opdrachten

---
+++ _private/views/homepage.php
@@ -0,0 +1,24 @@
+<?php $this->layout('layouts::website');?>
+
+<?php $this->start('sidebar')?>
+<!-- Door sections toe te voegen aan je layout kun je deze per pagina aanpassen -->
+<div class="top-10">
+	<h3>Meer weten over...</h3>
+	<ol>
+        <li><a href="https://www.transformers.community/" target="_blank">Transformers Community</a></li>
+        <li><a href="https://www.nji.nl/depressie" target="_blank">Depressie</a></li>
+        <li><a href="http://www.kenniscentrum-kjp.nl/ouders-jongeren/angst/" target="_blank">Angststoornissen</a></li>
+        <li><a href="http://www.kenniscentrum-kjp.nl/ouders-jongeren/gedragsstoornissen-odd-cd/" target="_blank">Gedragsstoornissen</a></li>
+        <li><a href="http://www.trimbos.nl/" target="_blank">Verslaving</a></li>
+	</ol>
+</div>
+<?php $this->stop()?>
+
+<h1><strong>Welkom bij de Transformers Community</strong></h1><br>
+
+<h3><strong>Wij zijn een groeiende beweging van jongeren die zich inzet voor mentale gezondheid.</strong></h3><br>
+<p>We leven in een samenleving waar onvoldoende ruimte is voor onze mentale gezondheid. Daar willen wij samen verandering in brengen! We zijn een community van jongeren die ervaringen en tips uitwisselt op het gebied van mentale gezondheid en persoonlijke ontwikkeling. Zo creëren we meer openheid en helpen we elkaar om te groeien.</p>
+
+
+
+
